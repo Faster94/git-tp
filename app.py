699425fd60035dc0cf8e639502d8388ffb8f0685
Faster94 <ysne94@gmail.com>
2025-03-19 10:59:33 +0100

    2ème branche
    test 2ème branche

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
+import logging
 
-print("Hello, Git!")
+
+print("Hello, Git!") # 0
+
+# Configuration de base des logs
+logging.basicConfig(
+    filename="app.log",  
+    level=logging.INFO,  
+    format="%(asctime)s - %(levelname)s - %(message)s",  
+)
+
+logging.info("Ceci est un log d'information.")
+logging.warning("Attention ! Quelque chose pourrait mal tourner.")
+logging.error("Erreur critique détectée !")
+
